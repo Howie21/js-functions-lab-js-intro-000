4c7bf15456ce6568f18f1debbed880c3541aee86
Wesley B. Howard <bryce9643@gmail.com>
2021-02-07 00:57:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+
+
+function happyHolidays() {
+  return ('Happy holidays!')
+}
+function happyHolidaysTo(name) {
+  var name = "Wes"
+  return (`Happy holidays, ${name}`)
+}
+function happyCustomHolidayTo(holiday, name) {
+  var holiday = 'Christmas'
+  var name = "Wes"
+    return (`Happy ${holiday}, ${name}!`)
+}
+function holidayCountdown(holiday, days) {
+  var holiday = "Christmas"
+  var days = ("10")
+    return (`It's ${days} days until ${holiday}!`)
+}
